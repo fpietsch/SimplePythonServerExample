70e7e1c730c0dcf21b69c502c118178a6be186bd
Fabian <pietsch.fabian@gmail.com>
2017-10-05 12:13:52 +0200

    Create server.py

---
+++ server.py
@@ -0,0 +1,18 @@
+# #!/usr/bin/python
+import socket
+
+HOST, PORT = '', 8888
+
+listen_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+listen_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+listen_socket.bind((HOST, PORT))
+listen_socket.listen(1)
+file = open("data.txt","r+") 
+response = file.read()
+print 'Serving HTTP on port %s ...' % PORT
+while True:
+    client_connection, client_address = listen_socket.accept()
+    request = client_connection.recv(1024)
+    print request
+    client_connection.sendall( response)
+    client_connection.close()
